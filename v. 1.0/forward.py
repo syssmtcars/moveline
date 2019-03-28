4ecad2169e681213597c6e23425d7047f2e12386
vanproskur <vanproskur@gmail.com>
2019-03-28 19:41:06 +0300

    first commit

---
+++ v. 1.0/forward.py
@@ -0,0 +1,38 @@
+#!usr/bin/env python3
+
+from ev3dev.ev3 import *
+from time import sleep
+
+#ISREADY
+
+#Инициализация переменных
+speed = 450
+delta = 0
+m1 = 0 #-port D
+m2 = 0 #-port A
+mr = MediumMotor('outD')
+ml = MediumMotor('outA')
+ls = ColorSensor('in1')
+rs = ColorSensor('in4')
+ls.mode = 'COL-REFLECT'
+rs.mode = 'COL-REFLECT'
+#ПИД регулятор
+try:
+    while True:
+        a = rs.value()
+        b = ls.value()
+        c = 2.0
+        delta = (a-b)*c
+        if delta <= 0:
+            ml.run_forever(speed_sp = speed*-1)
+            mr.run_forever(speed_sp = speed+delta)
+        else:
+            mr.run_forever(speed_sp = speed)
+            ml.run_forever(speed_sp = (speed-delta)*-1)
+        #sleep(50 / 1000)
+except KeyboardInterrupt:
+    pass
+
+finally:
+    mr.stop(stop_action = 'brake')
+    ml.stop(stop_action = 'brake')
---
+++ v. 1.0/left.py
@@ -0,0 +1,39 @@
+#!usr/bin/env python3
+
+from ev3dev.ev3 import *
+from time import sleep
+
+#Инициализация переменных
+speed = 450 # Скорость
+delta = 0 # Пустая переменная delta , которая будет использоваться в будущем.
+m1 = 0 #-port D
+m2 = 0 #-port A
+#Инициализация моторов и датчиков
+mr = MediumMotor('outD')
+ml = MediumMotor('outA')
+ls = ColorSensor('in1')
+rs = ColorSensor('in4')
+#Режим датчиков
+ls.mode = 'COL-REFLECT'
+rs.mode = 'COL-REFLECT'
+#Калибровка
+b = rs.value()
+#ПИД регулятор
+try:
+    while True:
+        a = ls.value()# Считывание датчика
+        c = 8 # Коэфицент
+        delta = (a-b)*c #Разница покозаний датчиков , умноженная на коэфицент.
+        if delta <= 0:
+            mr.run_forever(speed_sp = speed)
+            ml.run_forever(speed_sp = (speed+delta)*-1)
+        else:
+            mr.run_forever(speed_sp = speed-delta)
+            ml.run_forever(speed_sp = speed*-1)
+
+        sleep(50 / 1000)
+except KeyboardInterrupt:
+    pass
+finally:
+    mr.stop(stop_action = 'brake')
+    ml.stop(stop_action = 'brake')
